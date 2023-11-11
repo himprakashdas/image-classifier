dfe8130a3b48f8887d43c2b8bae14cc06de71d5e
himprakashdas <himprakash.work@gmail.com>
2023-11-11 16:55:03 +0530

    set project structure

---
+++ project_structure.lua
@@ -0,0 +1,25 @@
+Image-classifier/
+|-- data/
+|   |-- raw/
+|       |-- train/
+|       |-- validation/
+|       |-- test/
+|   |-- processed/
+|       |-- train/
+|       |-- validation/
+|       |-- test/
+|-- src/
+|   |-- __init__.py
+|   |-- data_preprocessing.py
+|   |-- model.py
+|   |-- train.py
+|   |-- evaluate.py
+|   |-- utils.py
+|   |-- exceptions.py
+|   |-- logging.py
+|-- notebooks/
+|   |-- exploratory_data_analysis.ipynb
+|-- config/
+|   |-- config.yaml
+|-- requirements.txt
+|-- main.py
